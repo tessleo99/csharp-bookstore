cb9232a88181c43d143e19f458c07bb53f69cf49
tessleo99 <t.napoletano9@gmail.com>
2023-04-12 15:02:18 +0200

    inizializzazione variabili

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//In questo esercizio vogliamo inserire in maniera “hardcoded”
+//(ossia scritte nel codice senza dover chiedere all’utente nessun input) tutte le informazioni che troviamo su Amazon di un libro
+
+
+
+string title = "L'ombra del vento";
+string author = "Carlos Ruiz Zafón";
+
+long ISBNcode = 8804750480;
+int pageNumber = 420;
+
+int BookWeight = 290; //grammi
+
+//Dimensioni 13.9 x 2.5 x 21.4 cm
+
+float bookLength = 13.9F;
+float bookHeighth = 2.5F;
+float bookDepth = 21.4F;
+Console.WriteLine(bookLength);
+
+float averageRating = 4.5F;
+
+int reviewNumber = 5296;
+
+bool kidleVersion = true;
+bool flexibleCover = true;
